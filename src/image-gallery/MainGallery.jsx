aae7ee41052101b6a053fbdcbfc7e0c1c37d3caa
Ashik465 <helloashikul@gmail.com>
2023-11-02 00:08:38 +0600

    implemented checkbox and delete in gallery

--- src/image-gallery/MainGallery.jsx
+++ src/image-gallery/MainGallery.jsx
@@ -3,23 +3,22 @@ import ImageGallery from "./components/ImageGallery";
 
 const MainGallery = () => {
   const [images, setImages] = useState([
-    { id: 0, url: "/public/images/image-11.jpeg", feature: false },
-    { id: 1, url: "/public/images/image-2.webp", feature: false },
-    { id: 2, url: "/public/images/image-3.webp", feature: false },
-    { id: 3, url: "/public/images/image-4.webp", feature: false },
-    { id: 4, url: "/public/images/image-5.webp", feature: false },
-    { id: 5, url: "/public/images/image-6.webp", feature: false },
-    { id: 6, url: "/public/images/image-7.webp", feature: false },
-    { id: 7, url: "/public/images/image-8.webp", feature: false },
-    { id: 8, url: "/public/images/image-9.webp", feature: false },
-    { id: 9, url: "/public/images/image-10.jpeg", feature: false },
-    { id: 10, url: "/public/images/image-1.webp", feature: false },
+    { id: 0, url: "/images/image-11.jpeg" },
+    { id: 1, url: "/images/image-2.webp" },
+    { id: 2, url: "/images/image-3.webp" },
+    { id: 3, url: "/images/image-4.webp" },
+    { id: 4, url: "/images/image-5.webp" },
+    { id: 5, url: "/images/image-6.webp" },
+    { id: 6, url: "/images/image-7.webp" },
+    { id: 7, url: "/images/image-8.webp" },
+    { id: 8, url: "/images/image-9.webp" },
+    { id: 9, url: "/images/image-10.jpeg" },
+    { id: 10, url: "/images/image-1.webp" },
   ]);
 
   return (
     <div className="min-h-screen p-4 bg-gray-100">
-      <div className="container mx-auto bg-white max-w-7xl">
-        <h1 className="mb-4 text-3xl font-bold">Responsive Image Gallery</h1>
+      <div className="container mx-auto bg-white max-w-7xl rounded-xl">
         <ImageGallery images={images} setImages={setImages} />
       </div>
     </div>
--- src/image-gallery/components/ImageGallery.jsx
+++ src/image-gallery/components/ImageGallery.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+import { FaCheckSquare } from "react-icons/fa";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ImageGallery = ({ images, setImages }) => {
+  const [checkedImages, setCheckedImages] = useState([]);
+
   const onDragEnd = (result) => {
     if (!result.destination) return;
 
@@ -11,38 +15,91 @@ const ImageGallery = ({ images, setImages }) => {
     setImages(items);
   };
 
+  const handleCheckboxChange = (imageId) => {
+    const isChecked = checkedImages.includes(imageId);
+    if (isChecked) {
+      setCheckedImages(checkedImages.filter((id) => id !== imageId));
+    } else {
+      setCheckedImages([...checkedImages, imageId]);
+    }
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="imageGallery" direction="horizontal">
-        {(provided) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            className="grid grid-cols-1 gap-4 p-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
+    <div>
+      <div className="flex justify-between items-center px-10 py-5">
+        <div>
+          {checkedImages.length > 0 ? (
+            <div className="flex gap-2 items-center justify-center">
+              <FaCheckSquare className="h-7 w-7" fill="blue" />
+              <p className="text-2xl font-bold">
+                {checkedImages.length} File Selected
+              </p>
+            </div>
+          ) : (
+            <h1 className=" text-2xl font-bold">Gallery</h1>
+          )}
+        </div>
+        {checkedImages.length > 0 && (
+          <button
+            className="px-4 py-2 text-red-500 font-bold text-xl hover:underline"
+            onClick={() => {
+              setImages(
+                images.filter((image) => !checkedImages.includes(image.id))
+              );
+              setCheckedImages([]);
+            }}
           >
-            {images.map((image, index) => (
-              <Draggable
-                key={image.id.toString()} 
-                draggableId={image.id.toString()} 
-                index={index}
-              >
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className="overflow-hidden border border-gray-300 rounded-md first:center first:row-span-2 first:col-span-2"
-                  >
-                    <img src={image.url} alt={`Image ${image.id}`} />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
+            Delete file
+          </button>
         )}
-      </Droppable>
-    </DragDropContext>
+      </div>
+      <hr className="py-5" />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="imageGallery" direction="horizontal">
+          {(provided) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="grid grid-cols-1 gap-4 px-10 pb-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
+            >
+              {images.map((image, index) => (
+                <Draggable
+                  key={image.id.toString()}
+                  draggableId={image.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="relative  border-2  border-gray-300 rounded-md md:first:col-span-2 md:first:row-span-2 md:first:border-[3px]"
+                    >
+                      <img src={image.url} alt={`Image ${image.id}`} />
+                      <div
+                        className={`absolute inset-0 bg-black  ${
+                          checkedImages.includes(image.id)
+                            ? "opacity-50 "
+                            : "hover:opacity-50 opacity-0 "
+                        }}`}
+                      >
+                        <input
+                          type="checkbox"
+                          className="absolute top-5 left-5 w-5 h-5 cursor-pointer"
+                          onChange={() => handleCheckboxChange(image.id)}
+                          checked={checkedImages.includes(image.id)}
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 };
 
